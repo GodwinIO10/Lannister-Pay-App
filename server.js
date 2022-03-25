9946aba281961bbca5a0e90970d154991375da06
Godwin Onumadu <66049832+GodwinIO10@users.noreply.github.com>
2022-03-25 22:53:37 +0100

    new commit

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
+// creating express application
 const express = require("express")
 const app = express()
-const cors = require("cors")
+
+
+// connecting to mongoDB
 require("dotenv").config()
 const connectDB = require("./config/db")
+connectDB()
+
+
+// choosing port to start server
 const port = process.env.PORT || 7000
+
+
+// importing custom modules
 const feesRoute = require("./routes/feesRoute")
 const computeTransactionFeeRoute = require("./routes/computeTransactionFeeRoute")
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
 
 
+// enabling parsing of json object in the body of a request
+const cors = require("cors")
 app.use(cors())
 app.use(express.json()) 
-connectDB()
 
-// TO CONFIRM SERVER IS HEALTHY
+
+// confirming server is healthy
 app.get("/", (req, res) => { 
     res.status(200).send({ code: 201, message: "Server is ok" })
 })
 
-// FEE SETUP ENDPOINT
-app.use("/fees", feesRoute)
 
-// FEE COMPUTATION ENDPOINT
+// specifying fee handler
+app.use("/fees", feesRoute)
+                           
+
+// specifying fee-computation handler
 app.use("/compute-transaction-fee", computeTransactionFeeRoute)
 
 
+// error-handling middlewares
 app.use(notFound)
 app.use(errorHandler)
 

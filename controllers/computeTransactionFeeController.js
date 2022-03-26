25f0c5bb1300131c38a85b2d197fb09375d7b4c6
Godwin Onumadu <66049832+GodwinIO10@users.noreply.github.com>
2022-03-26 21:59:53 +0100

    new commit

--- controllers/computeTransactionFeeController.js
+++ controllers/computeTransactionFeeController.js
@@ -3,38 +3,60 @@ const transactionFEE = require("../models/computeTransactionFeeModel")
 
 // POST /compute-transaction-fee/
 const computeTransactionFeeInfo = asyncHandler (async (req, res) => {
+        const { ID, Amount, Currency, CurrencyCountry, Customer, PaymentEntity } = req.body
+        
+        //const { feeID, feeCurrency, feeLocale, feeEntity, entityProperty, feeType, feeValue } = req.body
     
-        const { feeID, feeCurrency, feeLocale, feeEntity, entityProperty, feeType, feeValue } = req.body
+        const TxnIDExists = await transactionFEE.findOne({ ID }) // uses ID to check if Transaction ID already exists in DB
     
-        const TxnfeeExists = await transactionFEE.findOne({ feeID }) // uses feeID to check if feeID already exists in DB
-    
-        if (TxnfeeExists) {
+        if (TxnIDExists) {
             res.status(400)
-            throw new Error("Fee ID already exists!")
+            throw new Error("Transaction ID already exists!")
         }
+
+        TransactionAmount = Amount  // Setting Amount to TransactionAmount
     
-        const TxnFeeDetails = await transactionFEE.create({
-            feeID,
-            feeCurrency,
-            feeLocale,
-            feeEntity,
-            entityProperty,
-            feeType,
-            feeValue
-    
-        })
-    
-        if (TxnFeeDetails) {
-            console.log("Sucessful Entry!")
-            res.status(200).json({ 
-            
-            })
+        if (feeType == "FLAT") {
+            AppliedFeeValue == feeValue
+        }
+        else if (feeType == "PERC") {
+            feeValue = parseInt(percValue)
+            AppliedFeeValue == ((feeValue / 100 )* Amount)
+        }
+        else if (feeType == "FLAT_PERC") {
+            percValue = parseInt(percValue)
+            feeValue = parseInt(flatValue)
+            AppliedFeeValue == feeValue + ((percValue / 100)* Amount)
+        }
+        if (Customer.BearsFee == true) {
+            ChargeAmount = TransactionAmount + AppliedFeeValue 
         }
         else {
-            res.status(400)
-            throw new Error("Error occured!")
+            ChargeAmount = TransactionAmount
         }
-    })
+
+        SettlementAmount = TransactionAmount - AppliedFeeValue
+        
+        try {
+            const TxnFeeDetails = await transactionFEE.create({
+                AppliedFeeID,
+                AppliedFeeValue,
+                ChargeAmount,
+                SettlementAmount
+        
+            })
+        
+            if (TxnFeeDetails) {
+                res.status(200).json({ TxnFeeDetails })
+                console.log("Sucessful Entry!")
+                console.log(TxnFeeDetails)
+            }
+        }
+        catch (err){
+                res.status(400)
+                throw new Error("Error occured!")
+            }
+        })
     
 
 module.exports = { computeTransactionFeeInfo }
